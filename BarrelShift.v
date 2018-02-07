7735e7b04df885433e297f765d6589733ce4ab46
SophieHegarty <shegarty874@gmail.com>
2018-02-07 00:26:07 +0000

    comments added

--- BarrelShift.v
+++ BarrelShift.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Sophie Hegarty
 // 
 // Create Date:    18:14:51 02/06/2018 
 // Design Name: 
@@ -15,7 +15,7 @@
 //
 // Revision: 
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments: REFERENCE: Xilink Spartan - Version 3; Pong P. CHU page 73
 //
 //////////////////////////////////////////////////////////////////////////////////
 module BarrelShift(
@@ -27,11 +27,12 @@ module BarrelShift(
 
 always @*
  begin
-	 if(direction == 1)
+	 if(direction == 1) //if direction = 1, rotate left
 		case(amt)
+			//concatination used to manipulate bits into a paricular order
 			3'o0: y = a;
-			3'o1: y = {a[0], a[7:1]};
-			3'o2: y = {a[1:0], a[7:2]};
+			3'o1: y = {a[0], a[7:1]}; //place bit 0 ie LSB to position 'bit 7' followed by the original bits from MSB to bit 1
+			3'o2: y = {a[1:0], a[7:2]}; //place bit 0 & 1 to bit 7 & 6, follwed by original bits from MSB to bit 2 
 			3'o3: y = {a[2:0], a[7:3]};
 			3'o4: y = {a[3:0], a[7:4]};
 			3'o5: y = {a[4:0], a[7:5]};
@@ -39,8 +40,8 @@ always @*
 			default: y = {a[6:0], a[7]};
 		endcase
 	  else
-		  case(amt)
-				3'o0: y = a;
+		  case(amt) //if direction = 0, rotate right
+				3'o0: y = a; //repeat as above but in opposite direction
 				3'o1: y = {a[6:0], a[7]};
 				3'o2: y = {a[5:0], a[7:6]};
 				3'o3: y = {a[4:0], a[7:5]};
