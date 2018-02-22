eb849f1c431ab02b66ffca5422c359d8095accbb
SophieHegarty <shegarty874@gmail.com>
2018-02-22 15:22:40 +0000

    ALU - assignment 1

---
+++ ALU/Bitwise.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:20:11 02/15/2018 
+// Design Name: 
+// Module Name:    Bitwise 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Bitwise(a, b, x);
+	input wire [7:0] a;
+	input wire [7:0] b;
+	output wire [7:0] x;
+	
+	 
+
+endmodule
---
+++ ALU/Negative.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:19:02 02/15/2018 
+// Design Name: 
+// Module Name:    Negative 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Negative(in, x);
+	input wire [7:0] in;
+	wire [7:0] x_1;
+	output wire [7:0] x;
+	
+	Not negative_unit (.in(in), .x(x_1));
+
+	x = x_1 + ;
+
+endmodule
