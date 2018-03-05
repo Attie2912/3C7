d31329679d2c1a0b23eb2ac03e1c40814a86af1b
SophieHegarty <shegarty874@gmail.com>
2018-03-05 14:41:34 +0000

    updated

--- ALU/Bitwise.v
+++ ALU/Bitwise.v
@@ -23,6 +23,14 @@ module Bitwise(a, b, x);
 	input wire [7:0] b;
 	output wire [7:0] x;
 	
+	assign x[0] = a[0]^b[0];
+	assign x[1] = a[0]^b[0];
+	assign x[2] = a[0]^b[0];
+	assign x[3] = a[0]^b[0];
+	assign x[4] = ~in[4];
+	assign x[5] = ~in[5];
+	assign x[6] = ~in[6];
+	assign x[7] = ~in[7];
 	 
 
 endmodule
