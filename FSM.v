d01e9cd57995fa86c6634798d0af87f5c7de731b
SophieHegarty <shegarty874@gmail.com>
2018-03-14 14:28:23 +0000

    FSm

---
+++ FSM.v
@@ -0,0 +1,59 @@
+// Listing 5.4
+module FSM
+(
+input wire clk, reset,
+input wire level,
+output reg tick
+);
+	// symbolic state declaration
+	localparam A = 2'b00, B = 2'b01, C = 2'b10;
+	
+	// signal declaration
+	reg [1:0] state_next, state_reg;
+	
+		always @(posedge clk, posedge reset)
+		if (reset)
+		state_reg <= A;
+		else
+		state_reg <= state_next;
+
+		
+	// state regist
+		// next-state logic and output logic
+		always @*
+			begin
+			state_next = state_reg; // default state: the same
+			tick = 1'b0; // default output: 0
+		
+		case(state_reg)
+			 A : if (level)   
+			 begin
+			 state_next = B ;
+				//tick = 1'b1 ;
+			end
+			 B : if (~level)  
+			 begin
+			 state_next = C ;
+			 //tick = 1'b1 ;
+			 end
+			 C : 
+				  begin
+				  //tick = 1'b1;
+				  
+				  if(level)
+						begin
+						tick = 1'b1 ;
+						state_next = B ;
+						end
+						
+				  else    state_next = A ;
+				  
+				  end
+			 default :               
+						begin
+						tick = 1'b0 ;
+						state_next = A ;
+						end
+		endcase
+	end
+endmodule
---
+++ FSM_tb.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:43:41 03/14/2018 
+// Design Name: 
+// Module Name:    FSM_tb 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FSM_tb;
+
+	//Signals
+	reg clk, reset ;
+	reg level ;
+	wire tick ;
+   // instantiate the circuit under test
+   FSM uut
+      ( .clk(clk), .reset(reset), .level(level), .tick(tick));
+
+
+
+	//Initialise reg ports 
+	initial
+	begin
+	clk = 1'b0 ;
+	level = 1'b0;
+	end
+
+	always #20 clk = ~clk ;
+	
+	//Set initial reset
+	initial     
+	begin
+	reset = 1'b1 ;
+	#20 reset = 1'b0 ;
+	#200 reset = 1'b1;
+	#20 reset = 1'b0;
+	end
+
+	//Set occurance of sequence
+		initial
+		begin
+		#20     level = 1'b0 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b0 ; 
+		#20     level = 1'b1 ; 
+		#40     level = 1'b0 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b1 ; 
+		#20     level = 1'b0 ; 
+		#20     level = 1'b1 ; 
+
+		end
+endmodule 
+
