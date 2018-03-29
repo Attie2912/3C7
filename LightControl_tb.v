efcab1cbd917663347789b8382b8ce6d3f16fa34
SophieHegarty <shegarty874@gmail.com>
2018-03-29 15:56:20 +0100

    updated - not working:(

--- LightControl_tb.v
+++ LightControl_tb.v
@@ -20,26 +20,44 @@
 //////////////////////////////////////////////////////////////////////////////////
 module LightControl_tb;
 		//Signals
-	  wire red, yellow, green;
+	  wire red, yellow, green, tick;
 	  reg clk, reset;
 	  reg [3:0] timer;
-	  reg [1:0] state_next, state_reg; 
+	  reg [2:0] state_next, state_reg; 
 	  
 	  parameter Red=3'b000;
 	  parameter Yellow=3'b010;
 	  parameter Green=3'b100;
 	  
-	  LightControl uut (.clk(clk), .reset(reset), .red(red), .yellow(yellow), .green(green));
+	  LightControl uut (.clk(clk), .reset(reset), .tick(tick), .red(red), .yellow(yellow), .green(green));
 	  
 	  initial 
 	  begin     
-		 clk = 0;
-		 reset = 0;
-		 state_reg = Red;
-		 #40;
+	    //timer = 4'b0000;
+		  clk = 0;
+		  reset = 1'b1;
+		 //state_reg = Red;
 	  end
 	  
+	  always
+	  begin
+	  		 #100 reset = 1'b1;
+		 #20 reset = 1'b0;
+		#1000;
+	  end
+	  
+	  
 	  always #10 clk = ~clk;
+	  
+	  /*initial s
+	  begin
+	  state_reg = Red;
+	  #10 reset = 1;
+	  clk = 0;
+	  end
+	  
+	  always #5 clk = ~clk;*/
+	  
 	  
 endmodule
 
