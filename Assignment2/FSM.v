63be97a47031f9266d8b448a7d4f289eb512febb
SophieHegarty <shegarty874@gmail.com>
2018-04-04 11:58:58 +0100

    fsm

--- Assignment2/FSM.v
+++ Assignment2/FSM.v
@@ -19,8 +19,8 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module FSM(
-	input wire clk, reset,
-	output wire red, yellow, green, clk_out//, walk, flash, dont
+	input wire clk, reset, pedestrian, 
+	output wire red, yellow, green, clk_out, dont_walk, walk
 );
 wire [5:0] q_r, q_y, q_g;
 wire reset_count, max_tick, tick_r, tick_y, tick_g;
@@ -34,7 +34,7 @@ Max_Tick #(.M(4)) green_tick (.clk(clk_out), .max_tick(max_g), .q(q_g), .reset(r
 //Max_Tick #(.M(5)) counter_ped(.clk(out_clk), .max_tick(max_ped), .q(q_ped), .reset(reset_count));
 //counter count_unit (.clk(clk), .reset(reset_count), .max_tick(max_tick), .q(q));
 
-LightControl light (.clk(clk), .reset(reset), .max_r(max_r), .max_y(max_y), .max_g(max_g), .red(red), .yellow(yellow), .green(green));
+LightControl light (.clk(clk), .reset(reset), .max_r(max_r), .max_y(max_y), .max_g(max_g), .pedestrian(pedestrian),  .red(red), .yellow(yellow), .green(green), .walk(walk), .dont_walk(dont_walk));
 
 
 endmodule
